b0d9c56471348b5ecaac13cc645bf7b62606af84
HassenBenSlima <hassenbenslima@gmail.com>
2023-11-11 22:37:48 +0100

    add login and interceptor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,32 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {CustomersComponent} from "./customers/customers.component";
 import {AccountsComponent} from "./accounts/accounts.component";
 import {NewCustomerComponent} from "./new-customer/new-customer.component";
 import {CustomerAccountsComponent} from "./customer-accounts/customer-accounts.component";
+import {LoginComponent} from "./login/login.component";
+import {AdminTemplateComponent} from "./admin-template/admin-template.component";
 
 const routes: Routes = [
-  { path :"customers", component : CustomersComponent},
-  { path :"accounts", component : AccountsComponent},
-  { path :"new-customer", component : NewCustomerComponent},
-  { path :"customer-accounts/:id", component : CustomerAccountsComponent},
+  {path: "", redirectTo: "/login", pathMatch: "full"},
+  {path: "login", component: LoginComponent},
+  {
+    path: "admin", component: AdminTemplateComponent,
+    children: [
+      {path: "customers", component: CustomersComponent},
+      {path: "accounts", component: AccountsComponent},
+      {path: "new-customer", component: NewCustomerComponent},
+      {path: "customer-accounts/:id", component: CustomerAccountsComponent},
+    ]
+  },
+  //add interceptor :npm g interceptor interceptors/app-http
+
+
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
